76d5cf73b1c424563e27e1e4f361fe672507a86a
Matías Caricato <mattiascaricato@gmail.com>
2015-10-27 00:09:49 -0300

    Merge files

--- adding-values.js
+++
@@ -1,8 +0,0 @@
-var person = Object.create(null)
-
-person.firstName = 'John'
-person['lastName'] = 'Doe'
-
-var propertyKey = 'ag'
-
-person[propertyKey + 'e'] = 40
--- objects.js
+++ objects.js
@@ -1,4 +1,18 @@
-var person = Object.create(null) // creates an empty object
+// Creating an empty object
+var person = Object.create(null)
 
 person.name // undefined
 person.coolMethod() // undefined is not a function
+
+// Adding some properties
+person.firstName = 'John'
+person['lastName'] = 'Doe'
+
+var propertyKey = 'ag'
+
+person[propertyKey + 'e'] = 40
+
+// Adding methods
+person.fullName = function () {
+  return this.firstName + ' ' + this.lastName
+}
